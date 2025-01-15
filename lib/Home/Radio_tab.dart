0f1882ee400efc81bd53548bec6b4744e81284cb
Maryammahmou-d <mariammahmoud7766@gmail.com>
2025-01-15 15:28:17 +0300

    create Radio Tab (Without API)

--- lib/Home/Radio_tab.dart
+++ lib/Home/Radio_tab.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:islame_app/themeing.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blueAccent);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/radio_image.png"),
+        SizedBox.fromSize(size: Size(0, 20)),
+        Text(
+          "اذاعة القرآن الكريم",
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+        SizedBox.fromSize(size: Size(0, 20)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.skip_next,
+              size: 60,
+              color: MyThemeData.GoldColor,
+            ),
+            Icon(
+              Icons.play_arrow,
+              size: 80,
+              color: MyThemeData.GoldColor,
+            ),
+            Icon(
+              Icons.skip_previous,
+              size: 60,
+              color: MyThemeData.GoldColor,
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
